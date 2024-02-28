b6c68168cb96ec9f5f7291a3c0d7e394853a2565
nooriya2408 <nooriya2408@gmail.com>
2024-02-28 20:55:21 +0530

    editfile2

--- utils/db.js
+++ utils/db.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: "localhost",
+    user: "root",
+    password: "Nooriya@123",
+    database: "test"
   }); 
   
   
